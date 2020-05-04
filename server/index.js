91f4b18fe54607bf8ab80006ae6e934146624394
henryphamit <henryph.it@gmail.com>
2020-05-04 20:45:51 +1200

    Correct docker setup

--- server/index.js
+++ server/index.js
@@ -8,10 +8,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// Henry - will redirect all the non-api routes to react frontend
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, '../client','index.html'));
-});
 
 app.get("/", (req, res) => {
   res.send({ Project: "Virtual Classroom" });
